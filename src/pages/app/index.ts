b6f20bdfa253cf314f3f066b24638d4d8888ba79
Anastasia <nastia.shrai@gmail.com>
2022-12-21 09:11:53 +0200

    feat: implement hash routing

---
+++ src/pages/app/index.ts
@@ -0,0 +1,68 @@
+import Page from '../../core/templates/page';
+import Header from '../../core/components/header';
+import MainPage from '../catalog';
+import ErrorPage, { ErrorTypes } from '../error';
+import CartPage from '../cart';
+import Footer from '../../core/components/footer';
+import Main from '../../core/components/main';
+
+export const enum PageIds {
+  CatalogPage = 'catalog-page',
+  CartPage = 'cart-page',
+  ProductPage = 'product-page',
+  ErrorPage = 'error-page',
+}
+
+class App {
+  private static container: HTMLElement = document.body;
+  private static defaultPageId: string = 'current-page';
+  private header: Header;
+  private footer: Footer;
+  static main: Main;
+
+  static renderNewPage(idPage: string) {
+    const currentPageHTML = document.querySelector(`#${App.defaultPageId}`);
+    if (currentPageHTML) {
+      currentPageHTML.remove();
+    }
+    let page: Page | null = null;
+
+    if (idPage === PageIds.CatalogPage || idPage === undefined) {
+      page = new MainPage(idPage);
+    } else if (idPage === PageIds.CartPage) {
+      page = new CartPage(idPage);
+    } else {
+      page = new ErrorPage(idPage, ErrorTypes.Error_404);
+    }
+
+    if (page) {
+      const pageHTML = page.render();
+      pageHTML.id = App.defaultPageId;
+      App.main.appendNewPage(pageHTML);
+    }
+  }
+
+  private enableRouteChange() {
+    window.addEventListener('hashchange', () => {
+      const hash = window.location.hash.slice(1);
+      console.log('hashchange', hash);
+      App.renderNewPage(hash);
+    });
+  }
+
+  constructor() {
+    this.header = new Header('header', 'header');
+    App.main = new Main('main', 'main');
+    this.footer = new Footer('footer', 'footer');
+  }
+
+  start() {
+    this.enableRouteChange();
+    App.container.append(this.header.render());
+    App.container.append(App.main.render());
+    App.renderNewPage('/');
+    App.container.append(this.footer.render());
+  }
+}
+
+export default App;

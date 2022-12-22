3c2c7f15674adba651c0b126306cd18f5c20f9ba
Svetlana Korbut <tebba@yandex.ru>
2022-12-22 12:14:32 +0100

    feat:add plugin for webpack-config and add fonts in folder fonts and add this fonts in header

--- webpack.config.js
+++ webpack.config.js
@@ -3,33 +3,37 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, './src/index.ts'),
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      { test: /\.ts$/, loader: 'ts-loader' },
-      {
-        test: /.(svg|png|jpg)$/i,
-        type: 'asset/resource',
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.ts', '.js' /* other extentions */],
-  },
-  output: {
-    filename: 'index.js',
-    path: path.resolve(__dirname, './dist'),
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, './src/index.html'),
-      filename: 'index.html',
-    }),
-    new CleanWebpackPlugin(),
-  ],
+	entry: path.resolve(__dirname, './src/index.ts'),
+	mode: 'development',
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+			{ test: /\.ts$/, loader: 'ts-loader' },
+			{
+				test: /\.(png|jpg|svg|gif)$/,
+				use: ['file-loader']
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/,
+				use: ['file-loader']
+			}
+		],
+	},
+	resolve: {
+		extensions: ['.ts', '.js' /* other extentions */],
+	},
+	output: {
+		filename: 'index.js',
+		path: path.resolve(__dirname, './dist'),
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, './src/index.html'),
+			filename: 'index.html',
+		}),
+		new CleanWebpackPlugin(),
+	],
 };

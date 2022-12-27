5563aae7e8c1b15a8bd8faf89de65637e022e45b
Anastasia <nastia.shrai@gmail.com>
2022-12-27 17:02:12 +0200

    feat: add markup and styles for empty cart

--- src/pages/cart/index.ts
+++ src/pages/cart/index.ts
@@ -1,4 +1,6 @@
 import Page from '../../core/templates/page';
+import { PageIds } from '../app';
+import './index.css';
 
 class CartPage extends Page {
   static TextObject = {
@@ -11,7 +13,24 @@ class CartPage extends Page {
 
   render() {
     const title = this.createTitle(CartPage.TextObject.CartTitle);
-    this.container.append(title);
+    title.classList.add('cart__title');
+
+    const cartPage = <HTMLElement>document.createElement('section');
+    cartPage.classList.add('cart__wrapper');
+
+    const emptyCartMsg = <HTMLParagraphElement>document.createElement('p');
+    emptyCartMsg.classList.add('cart__text');
+    emptyCartMsg.innerText = 'Your cart is empty';
+
+    const backBtn = <HTMLButtonElement>document.createElement('button');
+    backBtn.classList.add('button', 'back__button');
+    backBtn.innerText = 'return to catalog';
+    backBtn.addEventListener('click', () => {
+      window.location.hash = PageIds.CatalogPage;
+    });
+
+    cartPage.append(title, emptyCartMsg, backBtn);
+    this.container.append(cartPage);
     return this.container;
   }
 }

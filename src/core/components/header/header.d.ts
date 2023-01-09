44b916ad2924c3d1b63f1293834df75569c59eeb
Anastasia <nastia.shrai@gmail.com>
2023-01-09 13:59:41 +0200

    fix: remove comments

--- src/core/components/header/header.d.ts
+++ src/core/components/header/header.d.ts
@@ -1,12 +1,3 @@
 declare module '*.png';
 declare module '*.jpg';
 declare module '*.svg';
-// declare module '*.svg?inline' {
-//   const content: any;
-//   export default content;
-// }
-
-// declare module '*.svg' {
-//   const src: string;
-//   export default src;
-// }

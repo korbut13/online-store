cfb15fa123c0fffcc58c785c62982057aec1dfe0
Svetlana Korbut <tebba@yandex.ru>
2023-01-10 07:58:46 +0100

    feat:work with button copy link

--- src/pages/catalog/index.ts
+++ src/pages/catalog/index.ts
@@ -304,8 +304,8 @@ class CatalogPage extends Page {
         <div class="container">
             <div class="sort-search">
                 <div class="reset-total">
-                    <button class="reset-total__clear-filters" > Reset filters </button>
-                    <button class="reset-total__copy-link" > Copy link </button>
+                    <button class="reset-total__clear-filters"> Reset filters </button>
+                    <button class="reset-total__copy-link"> Copy link </button>
                 </div>
                 <div class="sort-of-products">
                     <label for= "sort-of-products" > Sort options: </label>
@@ -593,6 +593,20 @@ class CatalogPage extends Page {
             this.replaceFilterString();
             window.location.hash = 'catalog-page';
         });
+
+        const copyLink = <HTMLButtonElement>this.container.querySelector('.reset-total__copy-link');
+        copyLink.addEventListener('click', () => {
+            const text = window.location.href;
+            navigator.clipboard.writeText(text);
+
+            copyLink.classList.add('copyLink');
+            copyLink.innerText = "Copied";
+            setTimeout(() => {
+                copyLink.innerText = "Copy link";
+                copyLink.classList.toggle('copyLink');
+            }, 1000)
+        })
+
 
 
         return this.container;
